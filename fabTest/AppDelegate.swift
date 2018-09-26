b4713e518d15d8ac0d3d87548d1f52e94d9ec290
Martin Ogg <martin.ogg@dabgaming.com>
2018-09-26 09:39:04 +0100

    crash after 5 seconds

--- fabTest/AppDelegate.swift
+++ fabTest/AppDelegate.swift
@@ -19,6 +19,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            
+            Crashlytics.sharedInstance().crash()
+        }
         return true
     }
 
